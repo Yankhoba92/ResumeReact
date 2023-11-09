e44f6ace44cdbbf2d10bb770f989e5412666196a
Yankhoba <bagayoko.yan@gmail.com>
2023-11-09 13:51:06 +0100

    🐛 scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,6 +42,8 @@ const Footer = () => {
           </div>
         </div>
       </footer>
+      <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     </>
   );
 };
